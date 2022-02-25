d957ab3fe57a63e83e309538a21bae5f333db9f5
john19294 <76793074+john19294@users.noreply.github.com>
2022-02-25 15:53:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup(){
+ pixelDensity(1.0)
+canvas=createCanvas(600,500)
+canvas.center()
+video=createCapture(VIDEO)
+video.hide()
+
+
+}
+function preload(){
+song1=loadSound("music.mp3")
+song2=loadSound("music2.mp3")
+
+
+}
+function draw(){
+image(video,0,0,600,500)
+
+
+
+
+
+
+
+
+
+}
+
